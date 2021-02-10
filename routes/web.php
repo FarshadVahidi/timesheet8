058ccbe93647c97f0e1487e53b6b2cd7f4b1a84e
farshad vahidifard <farshad.vahidifard@studenti.unimi.it>
2021-02-10 11:57:16 +0100

    route & view for each type of user

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\HourController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,7 +22,21 @@ Route::get('/', function () {
 
 Route::view('about', 'about');
 Route::view('contact', 'contact');
+//
+//Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+//    return view('dashboard');
+//})->name('dashboard');
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+//auth route for all
+Route::group(['middleware' => ['auth']], function() {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    Route::get('/addNewPerson', [RegisterController::class, 'index'])->name('register');
+    Route::post('/addNewPerson', [RegisterController::class, 'store']);
+
+    Route::get('/allHour', [HourController::class,'index'])->name('hou');
+
+    Route::get('/addNewHour',[HourController::class,'create'])->name('add');
+    Route::post('/daycare', [HourController::class,'store']);
+
+});
